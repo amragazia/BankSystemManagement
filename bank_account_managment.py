9f7b9f4cc9732843ba573c667cb47375f5b81b20
amragazia <moroabdelmonem2003@gmail.com>
2025-09-17 01:57:46 +0300

    Update bank_account_managment.py
    (Updated) OOP logic is isolated, and UI logic handles interaction.

--- bank_account_managment.py
+++ bank_account_managment.py
@@ -1,5 +1,7 @@
 # Bank Account System in OOP style
 
+import sys
+
 #? creating a bank account (class)
 class BankAccount:
 
@@ -8,54 +10,34 @@ class BankAccount:
         self.owner = owner
         self.balance = balance
 
-    def deposit(self):
-        while True:
-            amount = input("Enter amount to Deposit: ")
-            if amount.isnumeric():
-                amount = int(amount)
-                break
-            else:
-                print("Invalid Entry, Try again, (Q)Exit.")
-                if amount.upper() == "Q":
-                    return
+    def deposit(self, amount):
         self.balance += amount
-        print(f"${amount} has been deposited into your account ending in ..{self.account_number[8:12]}")
-        return amount
-
-    def withdraw(self):
-        while True:
-            amount = input("Enter amount to Withdraw: ")
-            if amount.isnumeric():
-                amount = int(amount)
-                break
-            else:
-                print("Invalid Entry, Try again, (Q)Exit.")
-                if amount.upper() == "Q":
-                    return
+        return self.balance
+    
+    def withdraw(self, amount):
         if amount > self.balance:
-            print("Insufficient Funds, Transaction declined.")
-        else:
-            self.balance -= amount
-            print(f"${amount} has been withdrawn from your account ending in ..{self.account_number[8:12]}, New balance: ${self.balance}")
-        return amount
+            return False
+        self.balance -= amount
+        return True
 
-
-    # def check_balance(self):
-    #     return self.balance
-
+    def check_balance(self):
+        return self.balance
+    
 #! \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 #? creating accounts (objects)
 acc1 = BankAccount("200320251234", "Amr", 5000)
 # acc2 = BankAccount("200320255678", "Mona", 6000)
 
+#Edits make it more of a UI version
 
 # Menu
 menu_dict = {
     "1" : "Show account Info",
     "2" : "Deposit",
     "3" : "Withdraw",
-    "4" : "Check Balance"
+    "4" : "Check Balance",
+    "Q" : "Exit"
 }
 
 for key, val in menu_dict.items():
@@ -69,18 +51,40 @@ while True:
     else:
         break
 
-methods_dict = {
-    "2" : acc1.deposit,
-    "3" : acc1.withdraw,
-}
+if prompt == "1":
+        print(f"Here's Your Account Info:\nACC#: {acc1.account_number}\nACC Holder: {acc1.owner}\nBalance: ${acc1.balance}")
+elif prompt == "2":
+    while True:
+        amount = input("Enter amount to Deposit: ")
+        if amount.isnumeric() and int(amount) > 0:
+            amount = int(amount)
+            break
+        else:
+            print("Invalid Entry, Try again, (Q)Exit.")
+            if amount.upper() == "Q":
+                sys.exit()
+    acc1.deposit(amount)
+    print(f"${amount} has been deposited into your account ending in ..{acc1.account_number[8:12]}, New balance: ${acc1.balance}")
 
-if prompt != "Q" and prompt in methods_dict.keys():
-    methods_dict[prompt]()
-elif prompt != "Q" and prompt not in methods_dict.keys():
-    if prompt == "1":
-        print(f"Here's Your Account Info => ACC#: {acc1.account_number}\nACC Holder:{acc1.owner}\nBalance: ${acc1.balance}")
-    elif prompt == "4":
-        print(f"Balance: ${acc1.balance}")
-else:
-    print("Exiting Bank System.....")
+elif prompt == "3":
+    while True:
+        amount = input("Enter amount to Withdraw: ")
+        if amount.isnumeric() and int(amount) > 0:
+            amount = int(amount)
+            break
+        else:
+            print("Invalid Entry, Try again, (Q)Exit.")
+            if amount.upper() == "Q":
+                sys.exit()
+    if not acc1.withdraw(amount):
+        print("Insufficient Funds, Transaction declined.")
+    else:
+        print(f"${amount} has been withdrawn from your account ending in ..{acc1.account_number[8:12]}, New balance: ${acc1.balance}")
+
+elif prompt == "4":
+    balance = acc1.check_balance()
+    print(f"You're available balance: ${balance}")
+
+elif prompt == "Q":
+    sys.exit()
 
